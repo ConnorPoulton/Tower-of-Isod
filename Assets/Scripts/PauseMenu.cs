ed3fefa9a5e6bb31bbd501b2380b71e92e342481
Connor.Poulton <poulton.cw@gmail.com>
2021-01-02 05:07:16 -0500

    added pause menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,37 @@
+﻿using PixelCrushers.DialogueSystem;
+using UnityEngine;
+
+public class PauseMenu : MonoBehaviour
+{
+    private bool isPaused = false;
+    public GameObject pauseMenu;
+    private void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.P))
+        {
+            isPaused = !isPaused;
+        }
+
+        if (isPaused)
+        {
+            Time.timeScale = 0;
+            DialogueManager.Pause();
+            pauseMenu.SetActive(true);
+        }
+        else
+        {
+            Time.timeScale = 1;
+            DialogueManager.Unpause();
+            pauseMenu.SetActive(false);
+        }
+    }
+
+    public void EndProgram()
+    {
+        Application.Quit();
+    }
+}
